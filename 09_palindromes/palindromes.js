a5e2a1597b646f2c4187a80111f81e35958c9ada
Daniel Teo <daniel.teo@datavizardry.com>
2025-01-02 11:03:56 +0100

    exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let textToEvaluate = text.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    return textToEvaluate === textToEvaluate.split('').reverse().join('');
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+console.log(palindromes('racecar')); // true;
+console.log(palindromes('Racecar')); // true;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+    let result;
+    if(typeof nth === 'string') {
+        nth = parseInt(nth);
+    }
 
+    if (nth < 0) return "OOPS";
+    if (nth === 0) return 0;
+    
+    let previousFirst = 1;
+    let previousSecond = 0;
+
+    for(let i = 2; i <= nth; i++) {
+        let current = previousFirst + previousSecond;
+        previousSecond = previousFirst;
+        previousFirst = current;
+    }
+    return previousFirst;
 };
 
 // Do not edit below this line
